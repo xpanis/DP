5cf3202460daad66c1b00bf092bf5f6188805632
xpanis <panis1994@gmail.com>
2018-04-23 16:10:20 +0200

    Move into Tests

--- Tests/Test_curve25519/Test_curve25519.ino
+++ Tests/Test_curve25519/Test_curve25519.ino
@@ -1,25 +1,10 @@
 #include <Curve25519.h>
 
-
 uint8_t f1[32];
 uint8_t k1[32];
 
 uint8_t f2[32];
 uint8_t k2[32];
-
-/*// Generate the secret value "f" and the public value "k".
-Curve25519::dh1(k, f);
-// Send "k" to the other party.
-...
-// Read the "k" value that the other party sent to us.
-...
-// Generate the shared secret in "k" using the previous secret value "f".
-if (!Curve25519::dh2(k, f)) {
-    // The received "k" value was invalid - abort the session.
-    ...
-}
-// The "k" value can now be used to generate session keys for encryption.
-...*/
 
 void setup() {
   // put your setup code here, to run once:
@@ -98,7 +83,4 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
- 
-
 }
