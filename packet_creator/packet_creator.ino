c278b90e7bed5ac154cd100dfa1c71cae000293b
xpanis <panis1994@gmail.com>
2018-04-24 19:43:57 +0200

    Fix Packet creator problems
    *problem with allocation of memory
    *Create test packets

--- packet_creator/packet_creator.ino
+++ packet_creator/packet_creator.ino
@@ -11,6 +11,7 @@ uint8_t salt[2] = {7,32};
 uint8_t hash[32];
 uint8_t key[32];
 uint8_t key_for_blake[32];
+byte * zeroes;
 Speck speck;
 
 int number_of_16_u_arrays = 0;
@@ -116,8 +117,6 @@ void convert_array_to_array_on_position(byte * data_array, uint8_t start_index, 
   int j = 0;
   for (int i= start_index; i < (start_index + input_data_size); i++)
   {
-    //Serial.print("zapis cisla: ");
-    //Serial.println(input_data[j]);
     convert_number_to_array_on_position(data_array, i, 1, input_data[j]);
     j++;
   }
@@ -169,15 +168,11 @@ int create_packet(byte ** packet_to_ret, byte * payload, int size_of_payload, bo
   if (is_crypted)
   {
     number_of_16_u_arrays = number_of_words_is(size_of_payload);
-    free(temp_msg_to_cipher);
-    free(input_parts_of_packet);
-    free(output_parts_of_packet);
     int size_of_temp_msg_to_cipher = size_of_payload + 4;
     
     alocate_msg_mem(&msg, ((16 * number_of_16_u_arrays) + 2));
     *packet_to_ret = msg;
     alocate_msg_mem(&temp_msg_to_cipher, size_of_temp_msg_to_cipher); //sign of size of payload: type + seq + 32bytes of hash
-    
     input_parts_of_packet = (uint8_t **) malloc(number_of_16_u_arrays * sizeof(uint8_t)); //allocate x times word (16bytes block) to cipher: input + output
     output_parts_of_packet = (uint8_t **) malloc(number_of_16_u_arrays * sizeof(uint8_t));
     for (int i = 0; i < number_of_16_u_arrays; i++)
@@ -193,11 +188,9 @@ int create_packet(byte ** packet_to_ret, byte * payload, int size_of_payload, bo
         output_parts_of_packet[i][j] = 0;
       }
     }
-    
     convert_number_to_array_on_position(temp_msg_to_cipher, 0, 2, type);
     convert_number_to_array_on_position(temp_msg_to_cipher, 2, 2, (long) seq_number);
     convert_array_to_array_on_position(temp_msg_to_cipher, 4, size_of_payload, payload); //set public key into msg
-  
     int index = 0;
     int stop_index = 16;
     for (int i = 0; i < number_of_16_u_arrays; i++)
@@ -210,15 +203,13 @@ int create_packet(byte ** packet_to_ret, byte * payload, int size_of_payload, bo
         }
       }
       stop_index += 16;
-    }
-    
+    }    
     free(temp_msg_to_cipher);
     speck.setKey(secret, 32); //with calculated cipher via DFH and set Speck key
     for (int i = 0; i < number_of_16_u_arrays; i++)
     {
       speck.encryptBlock(&output_parts_of_packet[i][0], &input_parts_of_packet[i][0]);
     }
-    
     index = 0;
     stop_index = 16;
     for (int i = 0; i < number_of_16_u_arrays; i++)
@@ -229,17 +220,21 @@ int create_packet(byte ** packet_to_ret, byte * payload, int size_of_payload, bo
       }
       stop_index += 16;
     }
-    
+
+    for (int i = 0; i < number_of_16_u_arrays; i++)
+    {
+      free(input_parts_of_packet[i]);
+      free(output_parts_of_packet[i]);
+    }
     free(input_parts_of_packet);
     free(output_parts_of_packet);
-    
     packet_to_checksum =  (uint16_t *) malloc((number_of_16_u_arrays * 16) * sizeof(uint16_t));
     for (int i = 0; i < (number_of_16_u_arrays * 16); i++)
     {
       packet_to_checksum[i] = (uint16_t) msg[i];
     }
     uint16_t checksum = sum_calc((number_of_16_u_arrays * 16), packet_to_checksum);
-    free(packet_to_checksum);    
+    free(packet_to_checksum);
     convert_number_to_array_on_position(msg, (number_of_16_u_arrays * 16), 2, (long) checksum); //set checksum into msg
     size_of_whole_packet = (16 * number_of_16_u_arrays) + 2;
   }
@@ -251,13 +246,13 @@ int create_packet(byte ** packet_to_ret, byte * payload, int size_of_payload, bo
     convert_number_to_array_on_position(msg, 0, 2, type);
     convert_number_to_array_on_position(msg, 2, 2, (long) seq_number);
     convert_array_to_array_on_position(msg, 4, size_of_payload, payload); //set public key into msg
-    
+    //free(packet_to_checksum);  
     packet_to_checksum =  (uint16_t *) malloc((size_of_payload + 4) * sizeof(uint16_t));
     for (int i = 0; i < (size_of_payload + 4); i++)
     {
       packet_to_checksum[i] = (uint16_t) msg[i];
     }
-    uint16_t checksum = sum_calc((size_of_payload + 4), packet_to_checksum);
+    uint16_t checksum = sum_calc((size_of_payload + 4), packet_to_checksum);    
     free(packet_to_checksum);    
     convert_number_to_array_on_position(msg, (size_of_payload + 4), 2, (long) checksum); //set checksum into msg
     size_of_whole_packet = (size_of_payload + 6);
@@ -269,44 +264,63 @@ int create_packet(byte ** packet_to_ret, byte * payload, int size_of_payload, bo
 
 void setup() {
   Serial.begin(9600); // serial start for help print to console
+  int size_of_packet;
 
-  
-  free(temp_msg);
-  int size_of_packet = create_packet(&temp_msg, public_key_of_server, sizeof(public_key_of_server), false, 1, 32);
-  Serial.print("Packet is: ");
-  for (int i = 0; i < size_of_packet; i++)
-    {
-      Serial.print(temp_msg[i]);
-      Serial.print(", ");
-    }
-    
+
   Serial.println("");
   Serial.println("");
-  
-  free(temp_msg);
-  size_of_packet = create_packet(&temp_msg, salt, sizeof(salt), true, 4, 57);
-  Serial.print("Packet is: ");
+  free(temp_msg);  
+  size_of_packet = create_packet(&temp_msg, public_key_of_server, sizeof(public_key_of_server), false, 1, 32);
+  Serial.print("Packet Reg Reply is: ");
   for (int i = 0; i < size_of_packet; i++)
     {
       Serial.print(temp_msg[i]);
       Serial.print(", ");
     }
 
-  Serial.println("");
-  Serial.println("");
   
+ 
+  Serial.println("");
+  Serial.println("");
+  free(temp_msg);  
+  size_of_packet = create_packet(&temp_msg, NULL, 0, true, 2, 59);
+  Serial.print("Packet ack ciphered with empty places + 0: ");
+  for (int i = 0; i < size_of_packet; i++)
+    {
+      Serial.print(temp_msg[i]);
+      Serial.print(", ");
+    }
+
+   
+  Serial.println("");
+  Serial.println("");
+  free(temp_msg);
+  size_of_packet = create_packet(&temp_msg, NULL, 0, true, 3, 59);
+  Serial.print("Packet nack ciphered with empty places + 0: ");
+  for (int i = 0; i < size_of_packet; i++)
+    {
+      Serial.print(temp_msg[i]);
+      Serial.print(", ");
+    }
+
+
+  Serial.println("");
+  Serial.println("");
   free(temp_msg);
   size_of_packet = create_packet(&temp_msg, NULL, 0, false, 2, 59);
-  Serial.print("Packet ack: ");
+  Serial.print("Packet ack NOT ciphered with empty places + 0: ");
   for (int i = 0; i < size_of_packet; i++)
     {
       Serial.print(temp_msg[i]);
       Serial.print(", ");
     }
 
+   
+  Serial.println("");
+  Serial.println("");
   free(temp_msg);
   size_of_packet = create_packet(&temp_msg, NULL, 0, false, 3, 59);
-  Serial.print("Packet nack: ");
+  Serial.print("Packet nack NOT ciphered with empty places + 0: ");
   for (int i = 0; i < size_of_packet; i++)
     {
       Serial.print(temp_msg[i]);
