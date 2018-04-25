3e0a3cd43f7923a9dd6167962cc91220683d0d37
xpanis <panis1994@gmail.com>
2018-04-25 15:11:42 +0200

    Test program for creating packets from dec to hex (to be send)

---
+++ Tests/Dec_to_Hex/Converter_new/Converter_new/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array_of_ints = new int[] {181, 95, 226, 188, 180, 13, 186, 93, 242, 88, 25, 253, 45, 42, 2, 185, 189, 62};
+            string[] array_of_hex = new string[array_of_ints.Length];
+            for(int i = 0; i < array_of_ints.Length; i++)
+            {
+                array_of_hex[i] = array_of_ints[i].ToString("X");
+            }
+
+            Console.WriteLine("Pole Hexa je: ");
+            for (int i = 0; i < array_of_ints.Length; i++)
+            {
+                Console.Write(array_of_hex[i] + " ");
+            }
+            
+        }
+    }
+}
